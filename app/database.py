d0b7283ebe3802437dfa7dbf7f2ea84493c8b849
Tami Takada <ttakada20@stuy.edu>
2022-03-10 08:33:00 -0500

    adding room id

--- app/database.py
+++ app/database.py
@@ -1,4 +1,5 @@
 import sqlite3
+import random
 
 
 def get_db():
@@ -9,19 +10,36 @@ def db_setup():
     db = get_db()
     c = db.cursor()
 
-    command = "CREATE TABLE IF NOT EXISTS rooms (floor INTEGER KEY, room_number INTEGER PRIMARY KEY, room_name TEXT NOT NULL, coordinates TEXT NOT NULL, room_info TEXT);"
+    command = "CREATE TABLE IF NOT EXISTS rooms (room_id INTEGER PRIMARY KEY, floor INTEGER KEY, room_number TEXT NOT NULL, room_name TEXT NOT NULL, coordinates TEXT NOT NULL, room_info TEXT);"
     c.execute(command)
 
     db.commit()
     db.close()
+    
+def generate_unique_id():
+	db = get_db()
+	c = db.cursor()
+	
+	room_id = 0
+	rooms = True
+	while rooms:
+		room_id = random.randint(1000, 9999)
+		command = "SELECT * FROM rooms WHERE room_id = ?"
+		rooms = c.execute(command, (room_id,)).fetchone()
+
+	db.close()
+	return room_id
 
 
 def add_room(floor, room_number, room_name, coordinates, room_info=""):
     db = get_db()
     c = db.cursor()
+    
+    room_id = generate_unique_id()
+    print(room_id)
 
-    command = "INSERT INTO rooms (floor, room_number, room_name, coordinates, room_info) VALUES (?, ?, ?, ?, ?)"
-    c.execute(command, (floor, room_number, room_name, coordinates, room_info))
+    command = "INSERT INTO rooms (room_id, floor, room_number, room_name, coordinates, room_info) VALUES (?, ?, ?, ?, ?, ?)"
+    c.execute(command, (room_id, floor, room_number, room_name, coordinates, room_info))
 
     db.commit()
     db.close()
@@ -39,45 +57,45 @@ def get_all_rooms_on_floor(floor):
     return rooms
 
 
-def get_room(room_number):
+def get_room(room_id):
     db = get_db()
     c = db.cursor()
 
-    command = "SELECT * FROM rooms WHERE room_number = ?"
-    room = c.execute(command, (room_number,)).fetchone()
+    command = "SELECT * FROM rooms WHERE room_id = ?"
+    room = c.execute(command, (room_id,)).fetchone()
 
     db.close()
 
     return room
 
 
-def set_room_info(room_number, info):
+def set_room_info(room_id, info):
     db = get_db()
     c = db.cursor()
 
-    command = "UPDATE rooms SET room_info = ? WHERE room_number = ?"
+    command = "UPDATE rooms SET room_info = ? WHERE room_id = ?"
     c.execute(command, (info, room_number))
 
     db.commit()
     db.close()
 
 
-def add_to_room_info(room_number, new_info):
-    current_info = get_room(room_number)[4]
+def add_to_room_info(room_id, new_info):
+    current_info = get_room(room_id)[4]
     if len(current_info) > 0:
         revised_info = current_info[:-1]
         revised_info += f", {new_info[1:]}"
-        set_room_info(room_number, revised_info)
+        set_room_info(room_id, revised_info)
     else:
-        set_room_info(room_number, new_info)
+        set_room_info(room_id, new_info)
 
 
-def delete_room(room_number):
+def delete_room(room_id):
     db = get_db()
     c = db.cursor()
 
-    command = "DELETE FROM rooms WHERE room_number = ?"
-    c.execute(command, (room_number,))
+    command = "DELETE FROM rooms WHERE room_id = ?"
+    c.execute(command, (room_id,))
 
     db.commit()
     db.close()
